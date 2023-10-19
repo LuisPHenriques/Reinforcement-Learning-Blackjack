b1f60d1113ea06a4c9507c8b7b1800b26bf3a722
LuisPHenriques <luishenriques20001@gmail.com>
2023-10-19 15:25:24 +0100

    Sarsa implemented inspired from gym Q-Learning class code

---
+++ src/models/Sarsa_class.py
@@ -0,0 +1,53 @@
+import numpy as np
+from collections import defaultdict
+
+
+class SarsaAgent:
+    def __init__(self, environment, learning_rate = 0.001, initial_epsilon = 0.1, epsilon_decay = 0.05, final_epsilon = 0.01, discount_factor = 0.95):
+        """Initialize a Reinforcement Learning agent with an empty dictionary
+        of state-action values (q_values), a learning rate and an epsilon.
+
+        Args:
+            learning_rate: The learning rate
+            initial_epsilon: The initial epsilon value
+            epsilon_decay: The decay for epsilon
+            final_epsilon: The final epsilon value
+            discount_factor: The discount factor for computing the Q-value
+        """
+        self.q_values = defaultdict(lambda: np.zeros(environment.action_space.n))
+        self.env = environment
+        self.lr = learning_rate
+        self.discount_factor = discount_factor
+
+        self.epsilon = initial_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.final_epsilon = final_epsilon
+
+        self.training_error = []
+
+    def get_action(self, obs: tuple[int, int, bool]) -> int:
+        """
+        Returns the best action with probability (1 - epsilon)
+        otherwise a random action with probability epsilon to ensure exploration.
+        """
+        # with probability epsilon return a random action to explore the environment
+        if np.random.random() < self.epsilon:
+            return self.env.action_space.sample()
+
+        # with probability (1 - epsilon) act greedily (exploit)
+        else:
+            return int(np.argmax(self.q_values[obs]))
+
+    def update(self, obs: tuple[int, int, bool], action: int, next_action: int, reward: float, terminated: bool, next_obs: tuple[int, int, bool]):
+        """Updates the Q-value of an action."""
+        temporal_difference = (
+            reward + self.discount_factor * self.q_values[next_obs][next_action] - self.q_values[obs][action]
+        )
+
+        self.q_values[obs][action] = (
+            self.q_values[obs][action] + self.lr * temporal_difference
+        )
+        self.training_error.append(temporal_difference)
+
+    def decay_epsilon(self):
+        self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
